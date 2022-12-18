f0f2cc53e895c9128ea7d13e8402e31a09a13307
St2r <st2_r@qq.com>
2022-12-18 12:23:35 +0800

    [optimize]: only import ga when production

--- applications/blog/app/layout.tsx
+++ applications/blog/app/layout.tsx
@@ -7,7 +7,6 @@ import {ReactNode} from 'react';
 import {ServiceProvider} from '@model/context';
 import {St2rMuiThemeProvider} from '@model/theme';
 import {enableStaticRendering} from 'mobx-react-lite';
-import Script from 'next/script';
 
 typeof window === 'undefined' && enableStaticRendering(true);
 
@@ -16,16 +15,7 @@ const RootLayout = ({
                     }: { children: ReactNode }) => {
   return (
     <html>
-    <head>
-      <title>Root Layout Title</title>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BT8Y8XQXP0"/>
-      <Script id="google-analytics" strategy='afterInteractive' dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-BT8Y8XQXP0');`
-      }}/>
-    </head>
+    <head/>
     <body>
     <ServiceProvider>
       <St2rMuiThemeProvider>
